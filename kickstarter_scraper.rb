696b555ca997c177430c811a706c7a08be163f12
Nataliya R <n.rudenko@gmail.com>
2019-03-28 20:13:56 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -15,7 +15,7 @@ kickstarter.css("li.project.grid_4").each do |project|
     :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
     :description => project.css("p.bbcard_blurb").text,
     :location => project.css("ul.project-meta span.location-name").text,
-    
+    :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
   }
 end
    projects   
