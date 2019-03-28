0556f4acdebd0df61511e3f59343e367aa6a0c70
Nataliya R <n.rudenko@gmail.com>
2019-03-28 19:25:34 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,18 @@
 # require libraries/modules here
+require 'pry'
+require 'nokogiri'
+
 
 def create_project_hash
-  # write your code here
+  html = File.read('fixtures/kickstarter.html')
+    kickstarter = Nokogiri::HTML(html)
+
+  project = {}
+
+
+
+
 end
+create_project_hash
+# projects: kickstarter.css("li.project.grid_4")
+# title: project.css("h2.bbcard_name strong a").text
